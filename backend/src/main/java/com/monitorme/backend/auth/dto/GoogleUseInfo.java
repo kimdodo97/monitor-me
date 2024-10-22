0ffac99890edb9c59ec7f3835bfd55f87877ab1c
kimdodo97 <eh4536@naver.com>
2024-10-22 19:53:02 +0900

    feat : 구글 소셜 유저 정보 응답 클래스 구현

---
+++ backend/src/main/java/com/monitorme/backend/auth/dto/GoogleUseInfo.java
@@ -0,0 +1,24 @@
+package com.monitorme.backend.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class GoogleUseInfo {
+    private String socialId;
+    private String email;
+
+    @JsonProperty("verified_email")
+    private String verifiedEmail;
+
+    private String picture;
+
+    @Builder
+    public GoogleUseInfo(String socialId, String email, String verifiedEmail, String picture) {
+        this.socialId = socialId;
+        this.email = email;
+        this.verifiedEmail = verifiedEmail;
+        this.picture = picture;
+    }
+}
