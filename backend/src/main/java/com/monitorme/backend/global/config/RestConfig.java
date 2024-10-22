4c81eefb5c57ba2b36b8c18e969da775c0f1e911
kimdodo97 <eh4536@naver.com>
2024-10-22 23:17:34 +0900

    fix : RestClient 빈 Builder 형태로 변경

--- backend/src/main/java/com/monitorme/backend/global/config/RestConfig.java
+++ backend/src/main/java/com/monitorme/backend/global/config/RestConfig.java
@@ -3,13 +3,18 @@ package com.monitorme.backend.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestConfig {
 
     @Bean
-    RestClient restClient() {
-        return RestClient.builder()
-                .build();
+    public RestClient.Builder restClient() {
+        return RestClient.builder();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
